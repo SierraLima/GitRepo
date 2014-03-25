663cddfedba409d8e928ab98e1ed56117dba1406
Maxime Michel <webmaster@maxime-michel.com>
2014-03-25 13:06:56 +0100

    Update

--- app/views/admin/teetimes.blade.php
+++ app/views/admin/teetimes.blade.php
@@ -7,8 +7,6 @@ img { max-width: 200px; height: auto; }
 
 </style>
 
-{{ $teetimes }}
-
 <script type="text/javascript">
 
 $(document).ready(function () {
@@ -42,6 +40,11 @@ $(document).ready(function () {
 			$('#date option[value="'+outputDate+'"]').attr("selected",true);
 	
 	}
+
+	var url = "{{ URL::action('GolfClubsController@getTeetimes') }}";
+	$("#date").change(function(e) {
+		window.location = url+'/'+$(this).val();
+	});
 		
 	var myJSONObject = 
 		{
@@ -110,25 +113,32 @@ $(document).ready(function () {
 				@for ($j = 7; $j <= 22; $j++)
 					<td>
 						<?php
-							// i = minutes && j = hour
-							// we just need to format them correctly
 							$treatedTeetimes = 0;
 
+							// i = minutes && j = hour
+							// we just need to format them correctly
 							for ($k = 0; $k < count($teetimes); $k++) {
 								// handling the date
 								$dt = DateTime::CreateFromFormat("Y-m-d H:i:s", $teetimes[$k]->date);
 								$hour = $dt->format('H');
 								$minutes = $dt->format('i');
-								// available tee-time
-								if($i*10==$minutes && str_pad($j, 2, "0", STR_PAD_LEFT)==$hour) {
-									if(isset($teetimes[$k]->price)) {
+								$year = $dt->format('Y');
+								// converting 03 to 3
+								$month = ltrim($dt->format('m'), '0');
+								$day = ltrim($dt->format('d'), '0');
+
+								// reserved tee-time
+								if($i*10==$minutes && str_pad($j, 2, "0", STR_PAD_LEFT)==$hour && $date==$year."-".$month."-".$day) {
+									if(count($teetimes[$k]->reservation)==1) {
+										$treatedTeetimes++;
+										echo "<a href='#' class='btn-circle btn-blue'>&nbsp;</a>";
+									}
+									// available tee-time
+									elseif(count($teetimes[$k]->reservation)==0) {
 										$treatedTeetimes++;
 										echo "<a href='#' class='btn-circle btn-green'>".round($teetimes[$k]->price).".-</a>";
 									}
 								}
-								// elseif : reservation
-								//<a href="#" class="btn-circle btn-blue">&nbsp;</a>
-
 							}
 
 							// displaying the rest
@@ -140,11 +150,56 @@ $(document).ready(function () {
 			</tr>
 		@endfor
 
-
-
-
 	</tbody>
 </table>
+
+<style type="text/css">
+.content {float: right; width:80%; padding-left: 10px;}
+#links {float: left; width:20%;margin-bottom:10px;}
+.invisible{style:display:none;}
+</style>
+
+<ul id="links" class="nav nav-pills nav-stacked">
+	<li class="active" id="manage"><a href="#">Manage price categories</a></li>
+	<li id="liberate"><a href="#">Liberate tee-times</a></li>
+	<li id="delete"><a href="#">Delete tee-times</a></li>
+</ul>
+<div id="content">
+	<div class="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempus sem ac sem molestie fringilla. Donec interdum, sapien eget placerat viverra, magna dui molestie diam, sit amet vulputate metus mi vel dui. Nam vel luctus ipsum, at dictum nibh. Pellentesque tristique facilisis ornare. Praesent non magna eget mauris ultricies venenatis. In quis sodales mi. Integer in nulla dictum, ultricies nibh sed, suscipit odio. Integer non volutpat libero. Curabitur sed tellus non nunc eleifend varius at ac magna. Phasellus laoreet leo est, ac adipiscing augue ultricies nec. Morbi sed leo vitae felis euismod pellentesque et vel velit.</div>
+	<div class="content">liberate</div>
+	<div class="content">delete</div>
+</div>
+
+<script type="text/javascript">
+$(document).ready(function () {
+	// hiding non active panes
+	$(".content").eq(1).hide();
+	$(".content").eq(2).hide();
+
+	$("#manage, #liberate, #delete").click(function(e) {
+		e.preventDefault();
+
+		// active class
+		$("li.active").removeClass("active");
+		if($(this).hasClass("active"))
+			$(this).removeClass("active");
+		else
+			$(this).addClass("active");
+
+		// hiding everything but the menu
+		$(".content").not(this).hide();
+
+		// showing the pane
+		var index = $(this).index();
+		$(".content").eq(index).show();
+
+	});
+
+});
+
+</script>
+
+<br style="clear:both;" />
 
 {{ Form::open(array('url'=>'golfclubs/teetimes')) }}
 	<input type="hidden" id="json" name="json" value="" />
