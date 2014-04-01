4cc69a7d520cbb75612741b90d6df91822487e3e
Raubaire <Robert@.(none)>
2014-04-01 09:49:09 +0200

    US5 DD

--- app/controllers/TeetimesController.php
+++ app/controllers/TeetimesController.php
@@ -3,14 +3,25 @@
 class TeetimesController extends BaseController {
 
 	protected $layout = "layouts.main";
+	
+	/**
+	 * The database table used by the model.
+	 */
+	public function __construct() {
+		$this->beforeFilter('csrf', array('on'=>'post'));
+		$this->beforeFilter('auth', array('only'=>array('getProfile')));
+	}
 
-	// routed from GET request to /users
-	public function getSearch()
-	{
+	/**
+	 * return search tee-time page
+	 */
+	public function getSearch(){
 		$this->layout->content = View::make('search')->with('teetimes', Teetime::all());
 	}
     
-    //return the reservation page
+    /**
+	 * return reservation page
+	 */
     public function getReservation(){
         $this->layout->content = View::make('reservation');   
     }
