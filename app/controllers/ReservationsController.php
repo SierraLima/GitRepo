b7ff8238658bbb50d5054feb8c6654875cfb180d
Raubaire <Robert@.(none)>
2014-04-14 09:31:36 +0200

    US7 DD

--- app/controllers/ReservationsController.php
+++ app/controllers/ReservationsController.php
@@ -1,18 +1,29 @@
 <?php
 
 class ReservationsController extends Controller{
-    
+	
+	protected $layout = "layouts.main";
+	
+	/**
+	 * The database table used by the model.
+	 */
+	public function __construct() {
+		$this->beforeFilter('csrf', array('on'=>'post'));
+	}
+
+	/**
+	 * Validate the reservation
+	 */    
     public function postCreate() {
+			
+		// validation has passed, save user in DB
+		$reservation = new Reservation;
+		
+		$reservation->numberplayer = Input::get('numberplayer');
+		$reservation->user_id = Input::get('user_id');
+		$reservation->teetime_id = Input::get('teetime_id');
+		$reservation->save();
 
-				
-			// validation has passed, save user in DB
-			$reservation = new Reservation;
-			$reservation->numberplayer = Input::get('numberplayer');
-			$reservation->user_id = Input::get('user_id');
-			$reservation->teetime_id = Input::get('teetime_id');
-
-			$reservation->save();
-
-			return Redirect::to('teetimes/endreservation')->with('message', 'Thanks for your reservation!');
-		}
+		return Redirect::to('teetimes/endreservation')->with('message', 'Thanks for your reservation!');
+	}
 }
